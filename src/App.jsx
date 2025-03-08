a0837ccdf6fd626f15363d3b1ce578fbdcbc176d
Akmat <sirazhidinovakmatali@gmail.com>
2025-03-08 16:18:50 +0600

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import { io } from 'socket.io-client';
 
-const socket = io('http://192.168.0.119:8080'); // Подключаемся к серверу
+
+const socket = io('http://192.168.0.105:8080'); // Подключаемся к серверу
 
 import './App.css'
 
 function App() {
     const [messages, setMessages] = useState([]);
     const [input, setInput] = useState('');
+    const [name, setName] = useState('');
+    const messagesEndRef = useRef(null);
 
     useEffect(() => {
         socket.on('message', (msg) => {
@@ -18,9 +21,13 @@ function App() {
         return () => socket.off('message'); // Очищаем слушатель при размонтировании
     }, []);
 
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+
     const sendMessage = () => {
-        if (input.trim()) {
-            socket.emit('message', input); // Отправляем сообщение на сервер
+        if (input.trim() && name.trim()) {
+            socket.emit('message', { msg: input, name }); // Отправляем сообщение на сервер
             setInput('');
         }
     };
@@ -29,15 +36,34 @@ function App() {
     <div className='wrapper'>
       <div className='chat-wrapper'>
         <div className='chat-messages'>
-            <div>
+            <div className='message-text'>
                 {messages.map((msg, index) => (
-                    <p key={index}>{msg}</p>
+                    <div
+                        key={index}
+                    >
+                        <p className='author'>{msg.name}</p>
+                        <p className='message-p'>{msg.msg}</p>
+                    </div>
                 ))}
+                <div ref={messagesEndRef} />
             </div>
         </div>
       </div>
+    <div>
+        <input
+            className='name-input'
+            type="text"
+            placeholder='Your name'
+            value={name} onChange={(e) => setName(e.target.value)}
+        />
+    </div>
       <div className='chat-textarea-block'>
-          <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+          <input
+              className='message-input'
+              placeholder='Enter message...'
+              type="text"
+              value={input} onChange={(e) => setInput(e.target.value)}
+          />
           <button onClick={sendMessage}>Отправить</button>
       </div>
     </div>
